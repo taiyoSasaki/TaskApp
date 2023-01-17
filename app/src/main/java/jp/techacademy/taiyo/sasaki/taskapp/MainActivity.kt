bfb9cea52c968d30e0b50dce33583738290dc94d
taiyo.Sasaki <taiyo.sasaki@ods.co.jp>
2023-01-17 15:31:29 +0900

    second commit

--- app/src/main/java/jp/techacademy/taiyo/sasaki/taskapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taiyo/sasaki/taskapp/MainActivity.kt
@@ -1,7 +1,8 @@
 package jp.techacademy.taiyo.sasaki.taskapp
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.RealmChangeListener
@@ -9,7 +10,10 @@ import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+const val EXTRA_TASK = "jp.techacademy.taiyo.sasaki.taskapp.TASK"
+
 class MainActivity : AppCompatActivity() {
+    //Realmクラスの実装
     private lateinit var mRealm: Realm
     private  val mRealmListener = object : RealmChangeListener<Realm> {
         override fun onChange(element: Realm) {
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //TaskAdapterクラスの実装
     private lateinit var mTaskAdapter: TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +29,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
         }
 
         //Realmの設定
@@ -39,17 +44,40 @@ class MainActivity : AppCompatActivity() {
         //ListViewをタップしたときの処理
         listView1.setOnItemClickListener { parent, view, position, id ->
             //入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         //ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
             //タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            //ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK") { _, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("cancel", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
             true
         }
-
-        //アプリ起動時に表示テスト用のタスクを作成する
-        addTaskForTest()
-
         reloadListView()
     }
 
@@ -71,17 +99,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
         mRealm.close()
-    }
-
-    private fun addTaskForTest() {
-        val task = Task()
-        task.title = "作業"
-        task.contents = "プログラムを書いてPUSHする"
-        task.date = Date()
-        task.id = 0
-        mRealm.beginTransaction()
-        mRealm.copyToRealmOrUpdate(task)
-        mRealm.commitTransaction()
     }
 
 }
